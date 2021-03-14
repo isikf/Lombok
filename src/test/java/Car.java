bc98a149d713cd1664961e09f01998816871bc31
isikf <f.isik@live.com>
2021-03-14 06:22:23 +0000

    Initial commit

---
+++ src/test/java/Car.java
@@ -0,0 +1,19 @@
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+//used for getter, setter and toString altogether
+//@Getter
+//@Setter
+//@ToString
+
+public class Car {
+    private int year;
+    private int door;
+    private String model;
+    private String make;
+
+
+
+}
